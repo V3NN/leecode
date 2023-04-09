2f9e018d4d24fb8da12ea0a464e65e92fd5ef554
ven <774777579@qq.com>
2023-04-09 16:57:51 +0800

    爬楼梯

---
+++ math/70_climbingStairs/climbStairs_test.go
@@ -0,0 +1,10 @@
+package __climbingStairs
+
+import "testing"
+
+func TestClimbStairs(t *testing.T) {
+	t.Log(climbStairs(3))
+	t.Log(climbStairs(5))
+	t.Log(climbStairs(1))
+	t.Log(climbStairs(2))
+}
