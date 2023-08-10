eb6689bdb363c304a07e5cd4dce55685ddd36ca0
V3NN <774777579@qq.com>
2023-08-10 16:09:03 +0800

    最长回文串

---
+++ string/409_longestPalindrome/longestPalindrome_test.go
@@ -0,0 +1,11 @@
+package __longestPalindrome
+
+import "testing"
+
+func TestLongestPalindrome(t *testing.T) {
+	t.Log(longestPalindrome("abc"))
+
+	t.Log(longestPalindrome("abccccdd"))
+
+	t.Log(longestPalindrome("aaaaaccc"))
+}
