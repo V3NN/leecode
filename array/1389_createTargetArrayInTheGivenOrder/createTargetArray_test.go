ca45d74be39551e12bafde3fbb5de28b39442ed3
V3NN <774777579@qq.com>
2024-04-30 16:10:48 +0800

    按既定顺序创建目标数组

---
+++ array/1389_createTargetArrayInTheGivenOrder/createTargetArray_test.go
@@ -0,0 +1,9 @@
+package __createTargetArrayInTheGivenOrder
+
+import "testing"
+
+func TestCreateTargetArray(t *testing.T) {
+	//t.Log(createTargetArray([]int{0, 1, 2, 3, 4}, []int{0, 1, 2, 2, 1}))
+	//t.Log(createTargetArray([]int{1, 2, 3, 4, 0}, []int{0, 1, 2, 3, 0}))
+	t.Log(createTargetArray([]int{1}, []int{0}))
+}
